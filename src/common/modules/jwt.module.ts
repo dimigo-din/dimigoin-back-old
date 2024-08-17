5d1978dcbe027cd8aafa7a73dfc128c41df6331e
noViceMin <admin@isamin.kr>
2024-08-18 01:48:19 +0900

    test: jwt

--- src/common/modules/jwt.module.ts
+++ src/common/modules/jwt.module.ts
@@ -9,14 +9,19 @@ import { DIMIConfigModule } from "./config.module";
 export const JWTOptions: JwtModuleAsyncOptions = {
   imports: [DIMIConfigModule],
   inject: [ConfigService],
-  useFactory: async (configService: ConfigService) => ({
-    publicKey: configService.get<string>("JWT_PUBLIC"),
-    privateKey: configService.get<string>("JWT_PRIVATE"),
-    global: true,
-    signOptions: {
-      algorithm: "RS256",
-    },
-  }),
+  useFactory: async (configService: ConfigService) => {
+    console.log("JWTOptions");
+    console.log("JWT_PUBLIC", configService.get<string>("JWT_PUBLIC"));
+    console.log("JWT_PRIVATE", configService.get<string>("JWT_PRIVATE"));
+    return {
+      publicKey: configService.get<string>("JWT_PUBLIC"),
+      privateKey: configService.get<string>("JWT_PRIVATE"),
+      global: true,
+      signOptions: {
+        algorithm: "RS256",
+      },
+    };
+  },
 };
 
 @Module({
